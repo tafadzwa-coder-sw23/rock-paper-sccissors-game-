3f47963829ea678f0529a2b1f332dc06af5c5c6d
tafadzwa-coder-sw23 <ernesttafadzwamaenda284@gmail.com>
2025-04-16 13:00:42 +0200

    Add files via upload

---
+++ rock,paper ,scissors/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+def get_choices():
+#    player_choice = input("Enter  choice(rock,paper,scissors:")
+#    computer_choice="paper"
+#    return  computer_choice
+# choices = {"player":"player_choice","computer":"computer_choice"}
+#      #return computer_choice
+
+# choices = get_choices()
+# print(choices)
+
+# dict ={"name","beau","color",choices}
